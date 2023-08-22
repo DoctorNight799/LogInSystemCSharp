e5260a6e0c3d4748588e7d50f6e5d2d80ebde77b
Last Night <72402692+DoctorNight799@users.noreply.github.com>
2023-08-22 21:18:44 +0300

    initial_commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogInSystem
+{
+    internal class Program
+    {
+        static bool isLoggedIn()
+        {
+            string username, password, un, pw;
+
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            StreamReader sr = File.OpenText(@"c:\data\" + username + ".txt");
+            un = sr.ReadLine();
+            pw = sr.ReadLine();
+            sr.Close();
+
+            if (un == username && pw == password){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int choice;
+
+            Console.Write("Make your choice.\n1:Registration\n2:LogIn\nYour choice: ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string username, password;
+
+                Console.Write("Enter username: ");
+                username = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+
+                if (!Directory.Exists(@"c:\data\"))
+                {
+                    Directory.CreateDirectory(@"c:\data\");
+                }
+
+                string path = (@"c:\data\" + username + ".txt");
+                if (!File.Exists(path))
+                {
+                    StreamWriter sw = File.CreateText(path);
+                    sw.WriteLine(username);
+                    sw.WriteLine(password);
+                    sw.Close();
+                    Main(args);
+                }
+                else
+                {
+                    Console.WriteLine("Already exist.");
+                    Main(args);
+                }
+            }
+            else if(choice == 2)
+            {
+                bool status = isLoggedIn();
+                if (!status)
+                {
+                    Console.WriteLine("Failed to login.");
+                    Console.ReadLine();
+                }
+                else if (status)
+                {
+                    Console.WriteLine("Succesfully logged in.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Error.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LogInSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogInSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("101f2b8b-0106-46f6-ab93-a1515e66ffa0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
